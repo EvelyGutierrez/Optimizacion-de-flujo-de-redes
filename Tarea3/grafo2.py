b89133e023079dac9e27f75f35b3677c82dc7009
EvelyGutierrez <35930437+EvelyGutierrez@users.noreply.github.com>
2018-04-05 15:58:54 -0500

    Add files via upload
    Haciendo pruebas con los algoritmos FordFulkerson y FloydWarshall grafos de igual o diferente tamaño

--- Tarea3/grafo2.py
+++ Tarea3/grafo2.py
@@ -29,6 +29,7 @@ class Grafo:
         self.E = []
         self.destino = None
         self.vecinos = dict()
+        self.i = None
         
 
     def creaNodos(self, orden): # creando los nodos
@@ -44,24 +45,25 @@ class Grafo:
                print(self.x[nodo], self.y[nodo], file=archivo)
         print(self.destino)
 
-    def conecta(self, prob):        
+    def conecta(self, prob):
+        self.i = 1
         for nodo in range(self.n - 1):
             for otro in range(nodo + 1, self.n):           
                 if random() < prob:                    
                     peso = choice(pesos)
-                    color = choice(colores)
+                    color = choice(colores)                    
                     if peso > 0:
-                        self.E.append((nodo, otro, peso, color))
+                        self.E.append((self.i, nodo, otro, peso, color))
+                        self.i += 1
                         if not nodo in self.vecinos: # vecindad de nodo
                             self.vecinos[nodo] = set()
                         if not otro in self.vecinos: # vecindad de otro
                             self.vecinos[otro] = set()    
                         self.vecinos[nodo].add((otro, peso))
                         self.vecinos[otro].add((nodo, peso))
-                        print(peso)
-                        
-        print(len(self.E))
-        print(self.vecinos)
+        return len(self.E)                                   
+        #print(self.E)
+        #print(self.vecinos)
         
 
     def grafica(self, plot): # imprimiendo el grafo con aristas
@@ -69,13 +71,14 @@ class Grafo:
         with open(plot, "w") as aristas:
             cabecera(aristas)
             num = 1
-            for (v, w, p, c) in self.E:                
+            for (num,v, w, p, c) in self.E:                
                 x1 = self.x[v]
                 x2 = self.x[w]
                 y1 = self.y[v]
                 y2 = self.y[w]              
-                flecha = "set arrow {:d} from {:f}, {:f} to {:f}, {:f} lt 5 lw {:f} lc rgb '{:s}' head".format(num,x1,y1,x2,y2,p,c)
-                print(flecha, file=aristas)
+                flecha = "set arrow {:d} from {:f}, {:f} to {:f}, {:f} lt 5 lw {:f} lc rgb '{:s}' nohead".format(num,x1,y1,x2,y2,p,c)
+                #flecha = "set arrow " + srt(num) + "from " + str(x1) + "," + str(y1) + " to " + str(x2) + "," + str(y2) + " lt 5 lw {:f} lc rgb '{:s}' head".format(num,p,c)
+                #print(flecha, file=aristas)
                 num += 1              
             pie(self.destino, aristas)
 
@@ -113,7 +116,7 @@ class Grafo:
         while len(cola) > 0:
             u = cola.pop(0)
             usados.add(u)
-            for (w, v, p, c) in self.E:
+            for (i,w, v, p, c) in self.E:
                 if w == u and v not in cola and v not in usados:
                     actual = f.get((u, v), 0)
                     dif = p - actual
@@ -150,74 +153,3 @@ class Grafo:
         print(maximo)
 
 
-
-    def GraficoCajaBigote(self):
-        import matplotlib.pyplot as plt 
-        
-        # Ejemplo de grafico de cajas en python
-
-        datos_1 = 1
-        datos_2 = 10
-        datos_3 = 20
-        datos_4 = 5
-
-        datos_graf = [datos_1, datos_2, datos_3, datos_4]
-
-        # Creando el objeto figura
-        fig = plt.figure(1, figsize=(9, 6))
-
-        # Creando el subgrafico
-        ax = fig.add_subplot(111)
-
-        # creando el grafico de cajas
-        bp = ax.boxplot(datos_graf)
-
-        # visualizar mas facile los atípicos
-        for flier in bp['fliers']:
-            flier.set(marker='o', color='red', alpha=0.5)
-        # los puntos aislados son valores atípicos
-
-    
-        #plt.ion()  # Nos ponemos en modo interactivo
-        #alt_esp = np.random.randn(100)+165 + np.random.randn(100) * 10  # Creamos unos valores para la altura de 100 españolas
-        #alt_ale = np.random.randn(100)+172 + np.random.randn(100) * 12  # Creamos unos valores para la altura de 100 alemanas
-        #alt_tai = np.random.randn(100)+159 + np.random.randn(100) * 9   # Creamos unos valores para la altura de 100 tailandesas
-        #plt.boxplot([alt_esp, alt_ale, alt_tai], sym = 'ko', whis = 1.5)  # El valor por defecto para los bigotes es 1.5*IQR pero lo escribimos explícitamente
-        #plt.xticks([1,2,3], ['Esp', 'Ale', 'Tai'], size = 'small', color = 'k')  # Colocamos las etiquetas para cada distribución
-        #plt.ylabel(u'Altura (cm)')
-
-
-
-    def GraficaErrorB(self):
-        # Definir tipo de salida para gnuplot
-        print("set term postscript eps color solid rounded blacktext “Helvetica” 20 enhanced")
-        # Nombre de los ejes
-        print("set xlabel “x”")
-        print("set ylabel “y”")
-
-        # Establecer rejilla
-        print("set grid")
-
-        # Establecer rango de los datos para el eje x e y
-        print("set yrange [0:40]")
-        print("set xrange [0:40]")
-
-        # Sitúa la leyenda abajo a la derecha
-        print("set key right bottom")
-
-        # Nombre del fichero en el que guardaremos la gráfica
-        print("set output ‘ejemplo_barras_errores.eps’")
-
-        # Función de ajuste para los datos
-        f = 20 + 10
-
-        # Ajuste de los datos según la función f
-        print("fit f(x) ‘datos.txt’ u 1:2 via a, b")
-
-        # Pintar la función, por un lado se pinta el ajuste y por otro los datos
-        print("plot {:f} lc rgb “blue”, \
-        ‘datos.txt’ u 1:2:3:4 with xyerrorbars lc rgb “red” notitle, \
-        ‘datos.txt’ u 1:2 with points pointtype 7 lc rgb “white” notitle, \
-        ‘datos.txt’ u 1:2 with points pointtype 6 lc rgb “red” title ‘x:y’".format(f))
-
-    
