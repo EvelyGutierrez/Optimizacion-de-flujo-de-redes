fbda7ca737bb2b9a17521b8150dbb0e2b62af2ba
EvelyGutierrez <35930437+EvelyGutierrez@users.noreply.github.com>
2018-04-07 18:26:53 -0500

    Pruebas de los Algoritmos
    Se probaron con varias corridas grafos con distintos tamaños y se almacenaron los tiempos de ejecucion de los algoritmos.

--- Tarea3/prueba.py
+++ Tarea3/prueba.py
@@ -1,47 +1,34 @@
 from grafo2 import Grafo
 from datetime import datetime
-instanteInicial = datetime.now()
-print("GRAFO 1")
-G1 = Grafo()
-G1.creaNodos(20)
-G1.imprimir("prueba.txt")
-E = G1.conecta(0.6)
-print("tamanno de E")
-print(E)
-G1.grafica("prueba.plot")
-d = G1.FloydWarshall()
-#print("Valor de d ")
-#print(d)
-a = G1.FordFulkerson(1,19)
-print("Tiempo")
-# al final de la partida
-instanteFinal = datetime.now()
-tiempo = instanteFinal - instanteInicial # Devuelve un objeto timedelta
-segundos = tiempo.seconds
-print(segundos)
+instanteInicial = datetime.now()# Tiempo Inicial 
+print("-------------------GRAFOS------------------")
 
+n = 20
+tiempoTotal = []
+cantidadCorridas = 10
 
-#G1.test(1,19)
-print("FordFulkerson desde el inicio hasta n ")
-print(a)
-
-#Prueba para otro grafo del mismo o distinto tamanno
-
-print("GRAFO 2")
-G2 = Grafo()
-G2.creaNodos(30)
-G2.imprimir("prueba.txt")
-E = G2.conecta(0.6)
-print("tamanno de E")
-print(E)
-G2.grafica("prueba.plot")
-#print(prueba.FloydWarshall())
-d = G2.FloydWarshall()
-#print("Valor de d ")
-#print(d)
-#a = G2.FordFulkerson(1,19)
-#tiempo = G2.TiempoEjecucion()
-print("Tiempo")
-print("FordFulkerson desde el inicio hasta n ")
-print(a)
-
+for t in range(cantidadCorridas):
+    G1 = Grafo()    
+    G1.creaNodos(n)
+    G1.imprimir("prueba.txt")
+    E = G1.conecta(0.6)
+    print("tamanno de E (conjunto de conexiones): ")
+    print(E)
+    G1.grafica("prueba.plot")
+    d = G1.FloydWarshall()
+    print("Resultado de FloydWarshall en tamanno del conjunto :")
+    print(d)
+    a = G1.FordFulkerson(1,n - 1)
+    # al final de la partida
+    instanteFinal = datetime.now() #Tiempo Final
+    tiempo = instanteFinal - instanteInicial # Devuelve un objeto timedelta
+    segundos = tiempo.seconds
+    print("FordFulkerson desde el inicio hasta n - 1 ")
+    print(a)
+    
+    n = n + 10
+    print("Valores de N mas 10 :")
+    print(n)
+    tiempoTotal.append(segundos)
+    
+print(tiempoTotal)
